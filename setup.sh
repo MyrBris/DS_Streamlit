ba5dc2f711abf61962171bf83f21ded54d72a2b1
MyrBris <94147902+MyrBris@users.noreply.github.com>
2025-04-17 13:00:09 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 pip install --upgrade pip
-pip uninstall numpy -y  # Force la réinstallation
-pip install --no-cache-dir --force-reinstall -r requirements.txt
+pip install --no-cache-dir --force-reinstall numpy==1.26.4 matplotlib==3.9.2 seaborn==0.13.2 pandas==2.2.3 scikit-learn==1.5.2 streamlit==1.32.2
